c25f6de3cfbcc0e358be9d3e333cf64b8972247f
Thalisson <thalissonmoura138@gmail.com>
2025-09-02 10:12:20 -0300

    :bug:fix: ajuste no layout e estilo da interface

--- src/ui/Spinner/index.jsx
+++ src/ui/Spinner/index.jsx
@@ -1,10 +1,9 @@
 import "./styles.css";
-import spinnerImage from "../../../public/img/spinner.gif";
 
 const Spinner = () => {
   return (
     <div className="overlay">
-      <img src={spinnerImage} alt="Carregando" className="spinner-image" />
+      <div className="spinner"></div>
     </div>
   );
 };
